53544204698f38e526e946d90bdbd31fda543553
tubersghaca571-eng <tubersghaca571@gmail.com>
2026-09-04 21:32:25 +0700

    fix: make connection.update callback async for QR code generation

--- bot.js
+++ bot.js
@@ -252,7 +252,7 @@ async function startBot() {
 
     sock.ev.on('creds.update', saveCreds);
 
-    sock.ev.on('connection.update', (update) => {
+    sock.ev.on('connection.update', async (update) => {
         const { connection, lastDisconnect, qr } = update;
         if (qr) {
             const qrPath = path.join(__dirname, 'qr.png');
